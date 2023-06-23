954c9818a641a19b6ff5da9ad14d65a7323f3354
keyproco <keyproco.rd@gmail.com>
2023-06-24 01:13:32 +0200

    WIP list the pods by the short lived token

---
+++ go.mod
@@ -0,0 +1,5 @@
+module devops
+
+go 1.20
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- webserver.go
+++ webserver.go
@@ -1,12 +1,51 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"github.com/joho/godotenv"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
-func listPods(w http.ResponseWriter, r *http.Request) {
+func listRunningPods() {
+	host := os.Getenv("HOST")
+	token := os.Getenv("TOKEN")
+	// TODO use the token mounted within the pod
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	url := host + "/api/v1/namespaces/devops-apps/pods"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Failed to create request:", err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Failed to send request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Failed to read response body:", err)
+		return
+	}
+
+	fmt.Println("Response:", string(body))
+}
+
+func podsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
@@ -17,15 +56,22 @@ func listPods(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	listRunningPods()
+
 	fmt.Fprintf(w, "Pods everywhere")
 }
 
 func main() {
-	// New code
-	http.HandleFunc("/", listPods)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	http.HandleFunc("/", podsHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":3000", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
